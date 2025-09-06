ebe8fcb7ae91b03c2385025964f6ac328398a59f
Kenn <kennethbtst25@gmail.com>
2025-09-06 11:17:07 +0800

    fix-issue

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,25 +1,14 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-// module.exports = function (app) {
-//   app.use(
-//     createProxyMiddleware({
-//       target:
-//         // "https://aclc-guidance-management-sys-7ae44f42e747.herokuapp.com:8080",
-//         "https://eyefit-store-c24fee073fdb.herokuapp.com:5000",
-//       changeOrigin: true,
-//       pathFilter: "/api",
-//       onBeforeSetupMiddleware: undefined,
-//       onAfterSetupMiddleware: undefined,
-//     })
-//   );
-// };
-
 module.exports = function (app) {
   app.use(
-    ["/api"],
     createProxyMiddleware({
       target:
-        "http://localhost:5000" ||
-        "https://eyefit-store-c24fee073fdb.herokuapp.com:5000"
+        // "https://aclc-guidance-management-sys-7ae44f42e747.herokuapp.com:8080",
+        "https://eyefit-shop-800355ab3f46.herokuapp.com:5000",
+      changeOrigin: true,
+      pathFilter: "/api",
+      onBeforeSetupMiddleware: undefined,
+      onAfterSetupMiddleware: undefined,
     })
   );
 };

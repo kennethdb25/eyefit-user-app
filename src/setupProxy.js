35c57ce25a15f19ac04542b0ed1b3908d1766af3
Kenn <kennethbtst25@gmail.com>
2025-09-06 09:44:55 +0800

    setup-proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,25 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware({
+//       target:
+//         // "https://aclc-guidance-management-sys-7ae44f42e747.herokuapp.com:8080",
+//         "https://eyefit-store-c24fee073fdb.herokuapp.com:5000",
+//       changeOrigin: true,
+//       pathFilter: "/api",
+//       onBeforeSetupMiddleware: undefined,
+//       onAfterSetupMiddleware: undefined,
+//     })
+//   );
+// };
+
 module.exports = function (app) {
   app.use(
+    ["/api"],
     createProxyMiddleware({
       target:
-        // "https://aclc-guidance-management-sys-7ae44f42e747.herokuapp.com:8080",
-        "https://eyefit-store-c24fee073fdb.herokuapp.com:5000",
-      changeOrigin: true,
-      pathFilter: "/api",
-      onBeforeSetupMiddleware: undefined,
-      onAfterSetupMiddleware: undefined,
+        "http://localhost:5000" ||
+        "https://eyefit-store-c24fee073fdb.herokuapp.com:5000"
     })
   );
 };

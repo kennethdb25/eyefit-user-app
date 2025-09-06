454b469e1030d48324b9447db4fb2029f26229bc
Kenn <kennethbtst25@gmail.com>
2025-09-06 08:48:55 +0800

    Check-realtime-change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -106,7 +106,7 @@ export default function Login(props) {
               loading={loading}
               className="bg-green-700 hover:bg-green-800"
             >
-              Sign in
+              Sign insss
             </Button>
           </Form.Item>
         </Form>
